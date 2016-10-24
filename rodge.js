130af62a171dc49e1850b497e932c1a47be0a39d
JodiCode <water@imgay.lgbt>
2016-10-24 19:54:05 +0100

    Create rodge.js

---
+++ rodge.js
@@ -0,0 +1,28 @@
+var e.lastMessage = message.getLast(), edit();
+var lastMessage = message.getLast();
+var lastContact = user.lastSentMessage(svr);
+var uReply = user.reply(lastContact);
+
+var wh = discord.servId(svr), webHooks(e);
+var wh.GitHub = discord.servId(svr), registerGithub(+key);
+// Rodgebot actions
+var prefix = "~"; // can be changed by ~pfix
+
+
+// commands
+
+new EventListener = rodgeBot(e);
+onEvent({
+  register new Event() = "commandSent";
+  var onCommandSent = rodgeBot.getContext([parse]){
+  commandData;Url:"discord://$svr/$channel/$msg";
+  var parse = p;
+  var command = rcmd;
+  p.msg.class[] = [1,2,3,4];
+  var class = rcmd.important[];
+  p.msg.cReturn(DATA_TEXT?D);
+  p.msg.function() {
+    rodgeBot.sendMessage($.class,$.context,$.authUser,$.roleLevel + "");
+  }
+  
+});
